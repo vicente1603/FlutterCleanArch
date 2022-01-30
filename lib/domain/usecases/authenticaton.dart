e973ebcde0ed42e35ee6b88514cce65872350444
Vicente Santiago <vicente.oliveira@mjv.com.br>
2022-01-30 18:43:19 -0300

    add authentication use case

---
+++ lib/domain/usecases/authenticaton.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_clean_arch/domain/entities/account_entity.dart';
+import 'package:meta/meta.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({required String email, required String password});
+}
